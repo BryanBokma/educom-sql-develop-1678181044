b06b2d8a427807e79fec5fde6ca7d8a6a853c4cc
Bryan Bokma <bryanbokma4@gmail.com>
2023-03-07 12:37:03 +0100

    opmaak aangepast#1

---
+++ sql/sql tutorial/SQL_Intro.sql
@@ -0,0 +1,32 @@
+-- Wat is SQL?
+
+-- SQL = Structured Query Language
+-- SQL lets you acces and manipulate databases
+
+-- Wat kun je met SQL?
+
+-- SQL can execute queries against a database
+-- SQL can retrieve data from a database
+-- SQL can insert records in a database
+-- SQL can update records in a database
+-- SQL can delete records from a database
+-- SQL can create new databases
+-- SQL can create new tables in a database
+-- SQL can create stored procedures in a database
+-- SQL can create views in a database
+-- SQL can set permissions on tables, procedures, and views
+
+-- SQL gebruiken voor je website
+
+-- An RDBMS database program (i.e. MS Access, SQL Server, MySQL)
+-- To use a server-side scripting language, like PHP or ASP
+-- To use SQL to get the data you want
+-- To use HTML / CSS to style the page
+
+-- RDBMS 
+
+-- RDBMS stands for Relational Database Management System.
+-- RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+-- The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+-- Look at the "Customers" table
+
